446cde16df819f3e1eed557d46541ef72982f190
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2024-01-02 12:31:02 +0000

    cors

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+const corsOption = {
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}
+app.use(cors(corsOption));
 
 
 const db = require("./models");
