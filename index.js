fc979922e543a4480290193288e2b70d175596cf
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2024-01-02 11:55:30 +0000

    test

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Running on port 5000.");
 });
+
+// // Export the Express API
+// module.exports = app;
+const express = require("express");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+
+// async function main() {
+//   await db.sequelize.sync({force:true})
+// }
+
+// main()
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  });
