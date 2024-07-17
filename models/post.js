00fccd4d3847c68d43c86afa5d33db8f28eb6c6e
gapirajat <rajat.shinde201@pccoepune.org>
2024-07-17 15:42:33 +0000

    post model

--- models/post.js
+++ models/post.js
@@ -4,83 +4,71 @@ const { Model} = require('sequelize');
 module.exports = (sequelize,DataTypes) => {
     class Post extends Model {   
       static associate(models) {
-        // Post.belongsTo(models.User, { foreignKey: 'email' });
+        Post.belongsTo(models.User, { foreignKey: 'email' });
       }
   
     }
    
     Post.init({
-        Post_Id:{
-          type:DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey:true,
-          autoIncrement: true
+      location: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      budget_l:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+      },
+      budget_h:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+      },
+      duration_l:{
+        type : DataTypes.INTEGER,
+        allowNull: false
+      },
+      duration_h:{
+        type : DataTypes.INTEGER,
+        allowNull: false
+      },
+      exp:{
+        type:DataTypes.INTEGER,
+        allowNull: false
+      },
+       skill:{
+        type: DataTypes.JSON,
+        allowNull: true
+       },
+       project_desc:{
+        type:DataTypes.STRING,
+        allowNull:false
+       },
+       project_size:{
+        type:DataTypes.CHAR,
+        allowNull: false
+       },
+       sector_p: {
+        type:DataTypes.STRING,
+        allowNull:false,
+        defaultValue: 'pending'
+       },
+       email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'email'
         },
-        Post_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-          },
-        business_type: {
-            type: DataTypes.STRING,
-            allowNull: false
-          },
-          location: {
-            type: DataTypes.STRING,
-            allowNull: false
-          },
-          maxBudget:{
-            type: DataTypes.INTEGER,
-            allowNull:false
-          },
-          minBudget:{
-          type: DataTypes.INTEGER,
-          allowNull:false
-          },
-          Duration :{
-            type : DataTypes.INTEGER,
-            allowNull: false
-          },
-          YearsOfExperience:{
-            type:DataTypes.INTEGER,
-            allowNull: false
-          },
-           skills:{
-            type: DataTypes.STRING,
-            allowNull: false
-           },
-           project_Description:{
-            type:DataTypes.STRING,
-            allowNull:false
-           },
-           req_list:{
-            type:DataTypes.STRING,
-            allowNull:false
-           },
-           category_chosen:{
-            type:DataTypes.STRING,
-            allowNull:false
-           },
-           industry_size:{
-            type:DataTypes.STRING,
-            allowNull: false
-           },
-           email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            references: {
-              model: 'users',
-              key: 'email'
-            }
-          }, 
-           CreatedAt: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW
-          },
-          UpdatedAt: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW
-          }
-    },{
+        primaryKey:true,
+      }, 
+       CreatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+      },
+      UpdatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+      }
+},{
         sequelize,
         modelName: 'Post',
         tableName: 'posts',
