a398efe96b468f3a624770dde1c13cafdf0cad4a
gapirajat <rajat.shinde201@pccoepune.org>
2024-07-22 14:26:34 +0000

    cors

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -6,6 +6,7 @@ const validateToken = (req, res, next) => {
   if (!accessToken) return res.json({ error: "User not logged in!" });
 
   try {
+    console.log(accessToken)
     const validToken = verify(accessToken, "importantsecret");
     req.user = validToken; //decodes the token's payload and attaches it to the req object
     //user infor from validToken
